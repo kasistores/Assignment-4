7763080d03b358df46840747cb1b2160cc619ea5
Kevin Asistores <kasistores@gmail.com>
2017-09-13 16:57:33 -0400

    finished assignment

--- server/config/express.js
+++ server/config/express.js
@@ -1,14 +1,14 @@
 var path = require('path'),  
-    express = require('express'), 
-    mongoose = require('mongoose'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    config = require('./config'),
-    listingsRouter = require('../routes/listings.server.routes'), 
-    getCoordinates = require('../controllers/coordinates.server.controller.js');
+  express = require('express'), 
+  mongoose = require('mongoose'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  config = require('./config'),
+  listingsRouter = require('../routes/listings.server.routes'), 
+  getCoordinates = require('../controllers/coordinates.server.controller.js');
 
 module.exports.init = function() {
-  //connect to database
+//connect to database
   mongoose.connect(config.db.uri);
 
   //initialize app
@@ -20,18 +20,22 @@ module.exports.init = function() {
   //body parsing middleware 
   app.use(bodyParser.json());
 
+  /* serve static files */
+  app.use('/', express.static(__dirname + '/../../client'));
+  app.use('/public', express.static(__dirname + '/../../public'));
+
+  /* use the listings router for requests to the api */
+  app.use('/api/listings', listingsRouter);
+
   /* server wrapper around Google Maps API to get latitude + longitude coordinates from address */
   app.post('/api/coordinates', getCoordinates, function(req, res) {
     res.send(req.results);
   });
 
-  /* serve static files */
-  
-
-  /* use the listings router for requests to the api */
-
-
   /* go to homepage for all routes not specified */ 
+  app.all('/*', function(req, res) {
+    res.sendFile(path.resolve('client/index.html'));
+  });
 
   return app;
 };  
